b5b6962e9f18952b03bf3e25410dea8abb61ba92
Aznlilly <10782738+Aznlilly@users.noreply.github.com>
2023-10-15 18:00:20 -0400

    Update songtitle.js

--- songtitle.js
+++ songtitle.js
@@ -13,11 +13,7 @@ async function songTitle(mountPoint) {
 }
 
 let res = () => {
-        document.getElementById("currentsongtitletext").textContent = songTitle(mountPoint);
-    }
-    .catch(function (err) {
-      console.log("Failed to fetch page: ", err);
-    });
+  document.getElementById("currentsongtitletext").textContent = songTitle(mountPoint);
 };
 
 const interval = setInterval(() => {
