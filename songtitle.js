709728779d16a02212f78e408e5768bf64785b64
Aznlilly <10782738+Aznlilly@users.noreply.github.com>
2023-10-15 19:22:20 -0400

    Update songtitle.js

--- songtitle.js
+++ songtitle.js
@@ -23,7 +23,7 @@ async function songTitle(mountPoint) {
 async function setTitle(mountPoint) {
   var title = "OFFLINE";
   title = await songTitle(mountPoint);
-  document.getElementById("currentsongtitletext").textContent = title + "            ";
+  document.getElementById("currentsongtitletext").textContent = title + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 }
 
 function sleep(fn, par) {
