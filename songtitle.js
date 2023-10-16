aaafcdca1f3850a3fbbc861558e95fb6a26befa9
Aznlilly <10782738+Aznlilly@users.noreply.github.com>
2023-10-15 21:09:58 -0400

    Update songtitle.js

--- songtitle.js
+++ songtitle.js
@@ -3,6 +3,9 @@ var mountPoint = window.location.pathname.split("/")[1];
 async function songTitle(mountPoint) {
   const response = await fetch("https://music.pixelhumble.com/status-json.xsl");
   icecast_stats = await response.json();
+  if (icecast_stats.icestats.source === undefined) {
+    return "OFFLINE";
+  }
   if (!Array.isArray(icecast_stats.icestats.source)) {
     if (icecast_stats.icestats.source.listenurl.indexOf(mountPoint) > 0) {
       return icecast_stats.icestats.source.title;
